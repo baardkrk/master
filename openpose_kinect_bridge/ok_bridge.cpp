7868bae8c5d54c764ee7a408fdd55333bc38d300
Baard-Kristian Krohg <baard.krohg@gmail.com>
2017-09-21 16:20:20 +0900

    added ok_bridge

---
+++ openpose_kinect_bridge/ok_bridge.cpp
@@ -0,0 +1,15 @@
+/*
+ * Create a bridge between kinect output and openpose.
+ * 
+ * - Choose input for openpose (depth/rgb image)
+ * - Publish estimated 3D joint angles of each person to appropriate ROS topic.
+ */
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+  std::cout << "OPENPOSE KINECT BRIDGE" << std::endl;
+  
+  return 0;
+}
